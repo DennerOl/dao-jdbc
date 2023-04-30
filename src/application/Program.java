1ffe5e971dfff39801b343d6f7b45e73d028eb31
DennerOl <dener155@hotmail.com>
2023-04-30 19:56:56 -0300

    Reusando a instanciacao

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,6 +16,8 @@ public class Program {
 
 // comunico com a interface e a fabrica de Dao que cria um SellerDao		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
